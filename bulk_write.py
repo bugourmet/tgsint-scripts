8db8dfc49f21f844cc7c08f884e430ec86d2aa63
runtimeterrorist <67608248+runtimeterrorist@users.noreply.github.com>
2022-07-31 17:47:41 +0200

    print

--- bulk_write.py
+++ bulk_write.py
@@ -7,10 +7,11 @@ db = client.get_database("database")
 collection = db.get_collection("persons")
 requesting = []
 
-with open("formatted.txt", 'r') as f: ##loads the formatted file (json) and populates the DB
+with open("formatted.txt", 'r') as f: #loads the formatted file (json) and populates the DB
      contents = json.loads(f.read())
      for jsonbj in contents:
          requesting.append(InsertOne(jsonbj))
-
+     
 result = collection.bulk_write(requesting)
+print("DONE")
 client.close()
