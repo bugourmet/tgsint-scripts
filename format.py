73118c208931e9bb60d2e3bd3d0006819fda716a
runtimeterrorist <67608248+runtimeterrorist@users.noreply.github.com>
2022-07-31 03:49:41 +0200

    added location, extra info update

--- format.py
+++ format.py
@@ -9,23 +9,21 @@ with open("unformatted.txt", "r",encoding="utf-8") as filestream:
         name = currentline[2]
         surname = currentline[3]
         sex = currentline[4]
-        extra = currentline[5] + currentline[6] # remove if error is thrown
-        
+        location = currentline[5]
+        extra = ' '.join(str(x) for x in currentline[6:9]) 
         data = {
                     "phonenum":phonenum,
                     "fbid":fbid,
                     "name":name,
                     "surname":surname,
                     "sex":sex,
+                    "location":location,
                     "extra":extra
                     }
                     
             
         finaldata.append(data)
         
-
-
-### this formats the json data and saves it to a new file
 with open("formatted.txt", 'w') as json_file:
     json.dump(finaldata, json_file, 
                         indent=4,  
